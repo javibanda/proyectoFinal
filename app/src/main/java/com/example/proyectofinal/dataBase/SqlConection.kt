7eb9dc4dde0d7d4c8612be9802ec56935315d353
@narutojavibanda <narutojavibanda@gmail.com>
2021-03-23 20:12:34 +0100

    Change variable rename conn to connection

--- app/src/main/java/com/example/proyectofinal/dataBase/SqlConection.kt
+++ app/src/main/java/com/example/proyectofinal/dataBase/SqlConection.kt
@@ -10,20 +10,19 @@ class SqlConection {
     object SqlConection{
 
         private val driverString = "jdbc:mysql://192.168.64.2:3306/finalProyect"
-        private lateinit var conn: Connection
-
+        private lateinit var connection: Connection
         fun getToken():Connection{
             try {
                 Class.forName("com.mysql.jdbc.Driver").newInstance()
-                conn = DriverManager.getConnection(driverString, "root", "")
+                connection = DriverManager.getConnection(driverString, "root", "")
             }catch (ex: SQLException){
                 Log.d(":::SQLExcepcion", ex.message.toString())
             }
-            return conn
+            return connection
         }
 
         fun disconect(){
-            this.conn.close()
+            this.connection.close()
         }
 
     }

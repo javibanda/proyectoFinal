cdb57eece7a4ba0d02bbf231e00405d2d0a45244
@narutojavibanda <narutojavibanda@gmail.com>
2021-03-23 20:13:14 +0100

    Delete spaces

--- app/src/main/java/com/example/proyectofinal/lists/citys/MainFragment.kt
+++ app/src/main/java/com/example/proyectofinal/lists/citys/MainFragment.kt
@@ -1,4 +1,4 @@
-package com.example.proyectofinal.fragments
+package com.example.proyectofinal.lists.citys
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -12,7 +12,11 @@ class MainFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
     }
+
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {

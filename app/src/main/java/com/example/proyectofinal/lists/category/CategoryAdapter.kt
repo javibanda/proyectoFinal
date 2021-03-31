3cb333e3141ecfe5e840c60361c7fa9f869a9fe2
@narutojavibanda <narutojavibanda@gmail.com>
2021-03-31 18:02:06 +0200

    Refactor names

---
+++ app/src/main/java/com/example/proyectofinal/lists/category/CategoryAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.proyectofinal.lists.category
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.proyectofinal.R
+import com.example.proyectofinal.data.KindProduct
+import com.example.proyectofinal.extensions.inflate
+import com.example.proyectofinal.extensions.loadUrl
+
+class CategoryAdapter(private val list: List<KindProduct>) : RecyclerView.Adapter<CategoryAdapter.KindProductViewHolder>() {
+
+
+
+    class KindProductViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+
+        val txtKindProduct = item.findViewById(R.id.nameKindProduct) as TextView
+        val imgKindProduct = item.findViewById(R.id.imgKindProduct) as ImageView
+        fun bindKindProduct(kindProduct: KindProduct){
+            txtKindProduct.text = kindProduct.getName().capitalize()
+            imgKindProduct.loadUrl(kindProduct.getUrl())
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KindProductViewHolder {
+        val view = parent.inflate(R.layout.item_category, false)
+        return KindProductViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: KindProductViewHolder, position: Int) {
+        val kindProduct = list[position]
+        holder.bindKindProduct(kindProduct)
+    }
+
+    override fun getItemCount(): Int = list.size
+}
---
+++ app/src/main/java/com/example/proyectofinal/lists/category/CategoryFragment.kt
@@ -0,0 +1,38 @@
+package com.example.proyectofinal.lists.category
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.proyectofinal.R
+import com.example.proyectofinal.dataBase.DataManager.DataManager.listKindProduct
+
+
+class CategoryFragment : Fragment() {
+
+    private lateinit var recView: RecyclerView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_category, container, false)
+        recView = view.findViewById(R.id.miRecycler)
+        return view
+    }
+
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(itemView, savedInstanceState)
+        val adaptador = CategoryAdapter(listKindProduct())
+        recView.apply {
+            layoutManager = LinearLayoutManager(activity)
+            setHasFixedSize(true)
+            adapter = adaptador
+        }
+    }
+
+    companion object {
+        fun newInstance() = CategoryFragment().apply {}
+    }
+}

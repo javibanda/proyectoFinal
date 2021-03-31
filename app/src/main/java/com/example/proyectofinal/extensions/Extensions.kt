3e67df83b8058679d61019f0270f3ff3ef940cce
@narutojavibanda <narutojavibanda@gmail.com>
2021-03-31 17:52:43 +0200

    File with fun ViewGroup.inflate and ImageView.loadUrl

---
+++ app/src/main/java/com/example/proyectofinal/extensions/Extensions.kt
@@ -0,0 +1,15 @@
+package com.example.proyectofinal.extensions
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.annotation.LayoutRes
+import com.squareup.picasso.Picasso
+
+fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = true): View =
+        LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+
+fun ImageView.loadUrl(url: String){
+    Picasso.with(context).load(url).into(this)
+}

3ad68ea560ff64e0359d1d0728052732456577dd
@narutojavibanda <narutojavibanda@gmail.com>
2021-04-06 21:14:54 +0200

    Delete spaces from Fragment2.kt

--- app/src/main/java/com/example/proyectofinal/fragments/Fragment2.kt
+++ app/src/main/java/com/example/proyectofinal/fragments/Fragment2.kt
@@ -23,9 +23,7 @@ class Fragment2 : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?

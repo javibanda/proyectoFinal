fcdcb35332a4f1065d97d5e59d97025eb26a1707
@narutojavibanda <narutojavibanda@gmail.com>
2021-04-06 21:14:11 +0200

    Adapt MainActivity.kt for JetPack Navigation

--- app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -1,30 +1,30 @@
 package com.example.proyectofinal
 
-import android.content.Intent
 import android.os.Bundle
 import android.os.StrictMode
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.proyectofinal.dataBase.DataManager.DataManager.listProduct
 import com.example.proyectofinal.fragments.Fragment2
-import com.example.proyectofinal.fragments.FragmentSwitch
 import com.example.proyectofinal.fragments.category.CategoryFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import java.util.*
 
 
-class MainActivity : AppCompatActivity(), FragmentSwitch {
+class MainActivity : AppCompatActivity(){
 
     val fragment = CategoryFragment.newInstance()
     val fragment2 = Fragment2.newInstance()
-    val listFragment = Stack<Fragment>()
-    val fm = supportFragmentManager
-
-    private lateinit var btnProduct: BottomNavigationView
 
 
-
+    private lateinit var bottonmNav: BottomNavigationView
+    lateinit var navController: NavController
+    lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,61 +34,35 @@ class MainActivity : AppCompatActivity(), FragmentSwitch {
         StrictMode.setThreadPolicy(policy)
 
         showFragment(fragment)
-        addNewFragment(fragment)
-        navigationItemSelected()
 
         Log.d(":::Product", listProduct().toString())
     }
 
-    private fun navigationItemSelected(){
-        btnProduct = findViewById(R.id.bottom_navigation_view)
-        btnProduct.setOnNavigationItemReselectedListener { item ->
-            when(item.itemId){
-                R.id.bottom_nav_favorites ->{
-                    //addNewFragment(fragment2)
-                    startActivity(Intent(this@MainActivity, Chekin::class.java ))
-                }
-
-                R.id.bottom_nav_music ->{
-
-                    //addNewFragment(fragment)
-
-                    //onBackPressedPopBackstack()
-                }
-            }
-        }
-
-    }
+//    private fun navigationItemSelected(){
+//        bottonmNav = findViewById(R.id.bottom_navigation_view)
+//        bottonmNav.setOnNavigationItemReselectedListener { item ->
+//            when(item.itemId){
+//                R.id.bottom_nav_favorites ->{
+//                    //addNewFragment(fragment2)
+//                    startActivity(Intent(this@MainActivity, Chekin::class.java ))
+//                }
+//
+//                R.id.bottom_nav_music ->{
+//
+//
+//
+//
+//                }
+//            }
+//        }
+//
+//    }
 
     fun showFragment(fragment: Fragment){
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frameLayoutMain, fragment)
         fragmentTransaction.commit()
     }
-
-    fun addNewFragment(fragment: Fragment){
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayoutMain, fragment)
-        fragmentTransaction.addToBackStack(null)
-        listFragment.push(fragment)
-        fragmentTransaction.commit()
-        Log.d(":::ListaFragments", listFragment.size.toString())
-        Log.d(":::Fragments reales", fm.backStackEntryCount.toString())
-    }
-
-    private fun clearFragments(){
-
-    }
-
-    fun onBackPressedPopBackstack(){
-        finish()
-    }
-
-    override fun switchCategoryProduct() {
-        fragment2
-    }
-
-
 }
 
 

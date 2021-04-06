13ffd53fe471c2438851747bcdc05e910ee62595
@narutojavibanda <narutojavibanda@gmail.com>
2021-04-06 21:13:04 +0200

    Add fun navOptions()

--- app/src/main/java/com/example/proyectofinal/extensions/Extensions.kt
+++ app/src/main/java/com/example/proyectofinal/extensions/Extensions.kt
@@ -5,6 +5,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.example.proyectofinal.R
 import com.squareup.picasso.Picasso
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = true): View =
@@ -12,4 +18,17 @@ fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = true): 
 
 fun ImageView.loadUrl(url: String){
     Picasso.with(context).load(url).into(this)
+}
+
+
+fun navOptions(): NavOptions {
+    val options = androidx.navigation.navOptions {
+        anim {
+            enter = R.anim.slide_in_right
+            exit = R.anim.slide_out_left
+            popEnter = R.anim.slide_in_left
+            popExit = R.anim.slide_out_right
+        }
+    }
+    return options
 }

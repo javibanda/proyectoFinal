c5ee82c5a1c552eb23c527e5f2dc597a849a5e4b
@narutojavibanda <narutojavibanda@gmail.com>
2021-04-14 10:50:46 +0200

    Create data class ProductImg.kt

---
+++ app/src/main/java/com/example/proyectofinal/data/ProductImg.kt
@@ -0,0 +1,10 @@
+package com.example.proyectofinal.data
+
+class ProductImg(
+        val id: Int,
+        val url: String
+){
+    override fun toString(): String {
+        return "ProductImg(id=$id, url='$url')"
+    }
+}

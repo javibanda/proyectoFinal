136a6deb64f40578810fafd9eb2166952b7acefb
@narutojavibanda <narutojavibanda@gmail.com>
2021-04-14 10:40:57 +0200

    Refactor ListPlatformFragment.kt

---
+++ app/src/main/java/com/example/proyectofinal/fragments/listPlatform/ListPlatformFragment.kt
@@ -0,0 +1,40 @@
+package com.example.proyectofinal.fragments.listPlatform
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.proyectofinal.R
+import com.example.proyectofinal.adapter.platform.PlatformRecyclerAdapter
+import com.example.proyectofinal.dataBase.DataManager.DataManager.listPlatform
+
+
+
+class ListPlatformFragment : Fragment() {
+    val args: ListPlatformFragmentArgs by navArgs()
+    private lateinit var recView: RecyclerView
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View?
+    {
+        val view = inflater.inflate(R.layout.fragment_list_platform, container, false)
+        recView = view.findViewById(R.id.myRecyclerPlatform)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(":::Args", args.idCategory.toString())
+
+        val platformAdapter = PlatformRecyclerAdapter(listPlatform(), this, args.idCategory)
+        recView.apply {
+            layoutManager = LinearLayoutManager(activity)
+            setHasFixedSize(true)
+            adapter = platformAdapter
+        }
+    }
+}

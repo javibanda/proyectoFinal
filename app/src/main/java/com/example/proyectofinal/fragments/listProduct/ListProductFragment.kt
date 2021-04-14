5a3044a79c3364edee1811fce51ac76236cb5144
@narutojavibanda <narutojavibanda@gmail.com>
2021-04-14 10:41:14 +0200

    Create ListProductFragment.kt

---
+++ app/src/main/java/com/example/proyectofinal/fragments/listProduct/ListProductFragment.kt
@@ -0,0 +1,47 @@
+package com.example.proyectofinal.fragments.listProduct
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.proyectofinal.R
+import com.example.proyectofinal.adapter.product.ProductRecyclerAdapter
+import com.example.proyectofinal.data.Product
+import com.example.proyectofinal.dataBase.DataManager.DataManager.getListProduct
+
+
+class ListProductFragment : Fragment() {
+    val args: ListProductFragmentArgs by navArgs()
+
+    private lateinit var recView: RecyclerView
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_list_product, container, false)
+        recView = view.findViewById(R.id.myRecyclerProduct)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val productAdapter = ProductRecyclerAdapter(getListProduct(args.idCategoryPlatform[0], args.idCategoryPlatform[1]), this)
+        recView.apply {
+            layoutManager = LinearLayoutManager(activity)
+            setHasFixedSize(true)
+            adapter = productAdapter
+        }
+    }
+
+//    private fun choseProductList(): List<Product>{
+//        val listProduct: List<Product>
+//        if(args.idCategoryPlatform == 0){
+//            return getListProduct()
+//        }else{
+//            return getListProduct()
+//        }
+//    }
+}

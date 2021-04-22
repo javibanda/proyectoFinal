d5589063eef9c0ee3cd39e44a9da56ef4e90fb57
javibanda <72160777+javibanda@users.noreply.github.com>
2021-04-22 14:12:55 +0200

    Add fun for RatingBar

--- app/src/main/java/com/example/proyectofinal/fragments/product/ProductFragment.kt
+++ app/src/main/java/com/example/proyectofinal/fragments/product/ProductFragment.kt
@@ -5,19 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
+import androidx.core.graphics.toColorInt
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.RecyclerView
 import com.example.proyectofinal.R
-import com.example.proyectofinal.data.Platform
 import com.example.proyectofinal.data.Product
-import com.example.proyectofinal.dataBase.DataManager
-import com.example.proyectofinal.dataBase.DataManager.DataManager.getListProduct
+import com.example.proyectofinal.data.User
+import com.example.proyectofinal.data.User.getIsConnected
+import com.example.proyectofinal.data.User.getUser
 import com.example.proyectofinal.dataBase.DataManager.DataManager.getProduct
+import com.example.proyectofinal.dataBase.DataManager.DataManager.isRated
 import com.example.proyectofinal.extensions.loadUrl
-import com.example.proyectofinal.fragments.listPlatform.ListPlatformFragmentArgs
-import com.example.proyectofinal.fragments.listProduct.ListProductFragmentArgs
+import com.example.proyectofinal.util.DialogLogIn.launchDialog
 
 class ProductFragment : Fragment() {
 
@@ -27,10 +28,15 @@ class ProductFragment : Fragment() {
     private lateinit var txtPrice1: TextView
     private lateinit var txtPrice2: TextView
     private lateinit var txtDetails: TextView
-    private lateinit var recyclerView : RecyclerView
-    private lateinit var imgProduct : ImageView
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var imgProduct: ImageView
+    private lateinit var btnBuy: Button
+    private lateinit var btnFav: Button
+    private lateinit var btnRating: Button
+    private lateinit var ratingBar: RatingBar
     private var product: Product? = null
 
+    private var action = ProductFragmentDirections
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -42,8 +48,11 @@ class ProductFragment : Fragment() {
         txtDetails = view.findViewById(R.id.txtDetails)
         txtPrice1 = view.findViewById(R.id.txtPrice1)
         txtPrice2 = view.findViewById(R.id.txtPrice2)
+        btnBuy = view.findViewById(R.id.btnBuyProduct)
+        btnFav = view.findViewById(R.id.btnFavProduct)
+        ratingBar = view.findViewById(R.id.ratingBar)
+        btnRating = view.findViewById(R.id.btnRating)
         return view
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -56,32 +65,67 @@ class ProductFragment : Fragment() {
 
         setPrice()
         setImg()
-
+        setColorPlatform()
+        listenerBtnBuy()
+        btnRatingClick()
     }
 
-    private fun setPrice(){
+
+    private fun btnRatingClick(){
+        btnRating.setOnClickListener {
+            if (!getIsConnected()){
+                setToast("Usuario desconectado")
+            }else if (!isRated(product!!, getUser())){
+                setToast("Ya has valorado este producto")
+            }
+        }
+    }
+
+    private fun setToast(string: String){
+        Toast.makeText(
+                context,
+                string,
+                Toast.LENGTH_LONG
+        ).show()
+    }
+
+    private fun listenerBtnBuy() {
+        btnBuy.setOnClickListener {
+            if (getIsConnected()){
+
+            }else{
+                NavHostFragment.findNavController(this).navigate(
+                        action.actionProductFragmentToLogInFragment(true)
+                )
+            }
+        }
+    }
+
+    private fun setColorPlatform(){
+        txtPlatform.setTextColor(product!!.platform.color.toColorInt())
+    }
+    private fun setPrice() {
         val price = (product!!.price - 0.05).toString()
         val arrayPrice = price.split('.')
         txtPrice1.text = arrayPrice[0] + ","
         txtPrice2.text = arrayPrice[1] + "€"
     }
 
-    private fun listImgIsEmpty(): Boolean{
+    private fun listImgIsEmpty(): Boolean {
         return product!!.listImg.isNotEmpty()
     }
 
-    private fun listImgSizeIsOne(): Boolean{
+    private fun listImgSizeIsOne(): Boolean {
         return product!!.listImg.size == 1
     }
 
-    private fun showImgView(){
+    private fun showImgView() {
         imgProduct.loadUrl(product!!.listImg.first().url)
-
     }
 
-    private fun setImg(){
-        if (listImgIsEmpty()){
-            if (listImgSizeIsOne()){
+    private fun setImg() {
+        if (listImgIsEmpty()) {
+            if (listImgSizeIsOne()) {
                 showImgView()
             }
         }

fd9b7ec5c6e8bcc5c607e319f779f02d4c276f93
javibanda <72160777+javibanda@users.noreply.github.com>
2021-04-27 18:12:58 +0200

    Create fun removeItem()

--- app/src/main/java/com/example/proyectofinal/adapter/cart/ListCartViewHolder.kt
+++ app/src/main/java/com/example/proyectofinal/adapter/cart/ListCartViewHolder.kt
@@ -1,17 +1,16 @@
 package com.example.proyectofinal.adapter.cart
 
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.proyectofinal.R
 import com.example.proyectofinal.adapter.base.BaseViewHolder
+import com.example.proyectofinal.adapter.base.OnItemClickListener
 import com.example.proyectofinal.data.cart.Cart
 import com.example.proyectofinal.data.cart.DataCart
 
-class ListCartViewHolder(itemView: View) : BaseViewHolder(itemView) {
-
+class ListCartViewHolder(itemView: View) : BaseViewHolder(itemView){
 
     private val txtName = itemView.findViewById<TextView>(R.id.txtNameCart)
     private val txtPlatform = itemView.findViewById<TextView>(R.id.txtPlatformCart)
@@ -19,38 +18,38 @@ class ListCartViewHolder(itemView: View) : BaseViewHolder(itemView) {
     private val btnAdd = itemView.findViewById<ImageView>(R.id.btnAddCart)
     private val txtCount = itemView.findViewById<TextView>(R.id.txtCountCart)
     private val txtPriceProduct = itemView.findViewById<TextView>(R.id.txtPriceCart)
+    private lateinit var listener: OnItemClickListener
 
-    fun bindCart(cart: DataCart, fragment: Fragment) {
-
+    fun bindCart(cart: DataCart, fragment: Fragment, listener: OnItemClickListener) {
+        this.listener = listener
         txtName.text = cart.getProductName()
         txtPlatform.text = cart.getProductPlatformName()
         txtPriceProduct.text = cart.getTotalPrice().toString()
         txtCount.text = cart.getQuantity().toString()
         clickAddOrRemoveOne(cart)
-
-
     }
 
     private fun clickAddOrRemoveOne(cart: DataCart) {
-
         btnAdd.setOnClickListener {
             addOrRemoveOne(cart, 1)
         }
-
         btnSubtract.setOnClickListener {
-            if (cart.getQuantity() != 1) {
+            if (cart.getQuantity() > 1) {
                 addOrRemoveOne(cart, -1)
+            }else{
+                removeItem()
             }
         }
     }
 
-    private fun addOrRemoveOne(cart: DataCart, add: Int) {
-        Cart.add(cart.getProduct(), add)
-        txtPriceProduct.text = cart.getTotalPrice().toString()
-        txtCount.text = cart.getQuantity().toString()
-
+    private fun removeItem(){
+            listener.onItemClick(adapterPosition)
     }
 
-
-
+    private fun addOrRemoveOne(cart: DataCart, add: Int) {
+        Cart.add(cart.getProduct(), add)
+        listener.onItemClick()
+        txtPriceProduct.text = cart.getTotalPrice().toString()
+        txtCount.text = cart.getQuantity().toString()
+    }
 }

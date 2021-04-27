e7949df739f627ba8cbc95939749dc8805b25bdc
javibanda <72160777+javibanda@users.noreply.github.com>
2021-04-27 18:10:15 +0200

    Implement interface OnItemClickListener.kt

--- app/src/main/java/com/example/proyectofinal/fragments/CartFragment.kt
+++ app/src/main/java/com/example/proyectofinal/fragments/CartFragment.kt
@@ -1,6 +1,8 @@
 package com.example.proyectofinal.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,39 +14,43 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.proyectofinal.R
 import com.example.proyectofinal.adapter.cart.ListCartRecyclerAdapter
+import com.example.proyectofinal.adapter.base.OnItemClickListener
 import com.example.proyectofinal.data.User.getIsConnected
 import com.example.proyectofinal.data.cart.Cart
+import com.example.proyectofinal.data.cart.Cart.getTotalPrice
 
 
-class CartFragment : Fragment() {
+class CartFragment : Fragment(), OnItemClickListener {
 
     private lateinit var txtNotLogInCart: TextView
     private lateinit var btnLogInCart: Button
     private lateinit var recyclerViewCart: RecyclerView
     private lateinit var btnBuyCart: Button
+    private lateinit var txtTotalCart: TextView
 
     private var action = CartFragmentDirections
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_cart, container, false)
-
         txtNotLogInCart = view.findViewById(R.id.txtNotLogInCart)
         btnLogInCart = view.findViewById(R.id.btnLogInCart)
         recyclerViewCart = view.findViewById(R.id.recyclerViewCart)
         btnBuyCart = view.findViewById(R.id.btnBuyCart)
+        txtTotalCart = view.findViewById(R.id.txtTotalCart)
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val cartAdapter = ListCartRecyclerAdapter(Cart.getListCart(), this)
+        val cartAdapter = ListCartRecyclerAdapter(Cart.getListCart(), this, this)
 
         recyclerViewCart.apply {
             layoutManager = LinearLayoutManager(activity)
             setHasFixedSize(true)
             adapter = cartAdapter
         }
+        updateTotalPrice()
         checkIsUserConnected()
         clickLogIn()
     }
@@ -61,12 +67,14 @@ class CartFragment : Fragment() {
         txtNotLogInCart.visibility = View.VISIBLE
         btnLogInCart.visibility = View.VISIBLE
         btnBuyCart.visibility = View.INVISIBLE
+        txtTotalCart.visibility = View.INVISIBLE
     }
 
     private fun hideLogIn(){
         txtNotLogInCart.visibility = View.INVISIBLE
         btnLogInCart.visibility = View.INVISIBLE
         btnBuyCart.visibility = View.VISIBLE
+        txtTotalCart.visibility = View.VISIBLE
     }
 
     private fun clickLogIn(){
@@ -75,8 +83,22 @@ class CartFragment : Fragment() {
                     action.actionCartFragmentToLogInFragment(true)
             )
         }
-
     }
 
+    override fun onItemClick(position: Int) {
+        Log.d(":::Test: ", position.toString())
+        Cart.remove(position)
+        recyclerViewCart.adapter?.notifyDataSetChanged()
+        updateTotalPrice()
+    }
 
+    override fun onItemClick() {
+        updateTotalPrice()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateTotalPrice(){
+        val totalPrice = getTotalPrice().toString()
+        txtTotalCart.text = "TOTAL:       $totalPrice€"
+    }
 }

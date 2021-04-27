89a014adfc3338e5a934d62f2ed95425da0d112e
javibanda <72160777+javibanda@users.noreply.github.com>
2021-04-27 10:36:34 +0200

    Create fun equals and hasCode in Product.kt

--- app/src/main/java/com/example/proyectofinal/data/Product.kt
+++ app/src/main/java/com/example/proyectofinal/data/Product.kt
@@ -23,4 +23,21 @@ data class Product(
                 " rating=$rating)," +
                 " numRating=$numRating)," +
                 " imgProduct=$listImg"
+
+        override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (javaClass != other?.javaClass) return false
+
+                other as Product
+
+                if (id != other.id) return false
+
+                return true
+        }
+
+        override fun hashCode(): Int {
+                return id
+        }
+
+
 }

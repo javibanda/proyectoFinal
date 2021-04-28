5ebd812e3f8ce47eb7c22899a466e59923452534
javibanda <72160777+javibanda@users.noreply.github.com>
2021-04-28 18:19:46 +0200

    Create var listOrder in User.kt

--- app/src/main/java/com/example/proyectofinal/data/User.kt
+++ app/src/main/java/com/example/proyectofinal/data/User.kt
@@ -3,6 +3,7 @@ package com.example.proyectofinal.data
 object User {
 
     private var user: Person? = null
+    private var listOrder = ArrayList<Order>()
 
     fun connectedUser(person: Person?) {
         user = person

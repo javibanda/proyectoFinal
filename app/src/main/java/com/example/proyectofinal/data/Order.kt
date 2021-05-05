3d7073ee43cf56475e6e9514944306f44d371a2d
javibanda <72160777+javibanda@users.noreply.github.com>
2021-05-05 10:20:37 +0200

    Refactor code

--- app/src/main/java/com/example/proyectofinal/data/Order.kt
+++ app/src/main/java/com/example/proyectofinal/data/Order.kt
@@ -1,6 +1,15 @@
 package com.example.proyectofinal.data
 
-import com.example.proyectofinal.data.cart.Cart
+import com.example.proyectofinal.util.Date
 
-class Order(val id: Int, val date: String, val cart: Cart, val city: City) {
+class Order(val id: Int,
+            val date: Date,
+            val priceProducts: Float,
+            val priceDelivery: Float,
+            val person: Person,
+            val city: City
+            ) {
+    override fun toString(): String {
+        return "Order(id=$id, date=$date, priceProducts=$priceProducts, priceDelivery=$priceDelivery, person=$person, city=$city)"
+    }
 }

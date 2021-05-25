cec5b7de5143133360cff55d89912f5b05b69378
JAVA <narutojavibanda@gmail.com>
2021-05-25 08:44:09 +0200

    Add getters on User

--- app/src/main/java/com/example/proyectofinal/data/User.kt
+++ app/src/main/java/com/example/proyectofinal/data/User.kt
@@ -1,5 +1,7 @@
 package com.example.proyectofinal.data
 
+import android.media.Rating
+
 object User {
 
     private var user: Person? = null
@@ -21,4 +23,20 @@ object User {
     fun getIsConnected(): Boolean {
         return user != null
     }
+
+    private fun getSecondLastName(): String {
+        return if (user!!.secondLastName == null){
+            ""
+        }else{
+            user!!.secondLastName!!.toLowerCase().capitalize()
+        }
+    }
+
+    fun getCity() = user!!.city.name.capitalize() + ", " + user!!.city.region.capitalize()
+
+    fun getEmail() = user!!.email
+
+    fun getName() = user!!.name.toLowerCase().capitalize() + " " + user!!.lastName.toLowerCase().capitalize() + " " + getSecondLastName()
+
+    fun getDni() = user!!.dni.toUpperCase()
 }
